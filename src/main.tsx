c3a16b95666a332b846eebd0c814bd893b73e9c7
Miraz Ahmed <ahmedmiraz87@gmail.com>
2025-09-19 06:01:55 +0600

    createReducer

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {Provider} from "react-redux"
 import App from './App.tsx'
 import store from './state/store.ts'
+import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- src/state/store.ts
+++ src/state/store.ts
@@ -1,9 +1,12 @@
 
 import {configureStore} from '@reduxjs/toolkit'
+import counterReducer from './feature/Counter/CounterSlice'
 
 
 const store = configureStore({
-    reducer:{},
+    reducer:{
+        counter:counterReducer
+    },
 })
 
 export default store;
